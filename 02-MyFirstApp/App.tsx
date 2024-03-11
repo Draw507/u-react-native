8dcdf2deaa89c137792c7071a7fd9a6f6dbcff86
David Robinson <drobinson@assanet.com>
2024-03-10 22:37:10 -0500

    add 02-first-app

---
+++ 02-MyFirstApp/App.tsx
@@ -0,0 +1,10 @@
+import {SafeAreaView} from 'react-native';
+import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
+
+export const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <HelloWorldScreen name='David Robinson' />
+    </SafeAreaView>
+  );
+};
